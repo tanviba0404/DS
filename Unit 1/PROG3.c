da72e4fc0ee7a5b8332bf684045abff16742c1be
tanviba0404 <tanvibazala55@gmail.com>
2026-07-17 12:37:33 +0530

    Add files via upload

---
+++ Unit 1/PROG3.c
@@ -0,0 +1,34 @@
+/* Create arrays A,B and C of size 3, perform C=A+B.
+Name:Daksh Zala
+Enrollment no.:92500527106 */
+
+#include<stdio.h>
+
+void main()
+{
+     int a[3],b[3],c[3],i;
+
+     for(i=0;i<3;i++)
+     {
+         printf("\n Enter value for a %d :",i+1);
+         scanf("%d",&a[i]);
+     }
+
+     for(i=0;i<3;i++)
+     {
+         printf("\n Enter value for b %d :",i+1);
+         scanf("%d",&b[i]);
+     }
+
+     for(i=0;i<3;i++)
+     {
+         c[i] = a[i] + b[i];
+     }
+
+     printf("Array c (a+b) is : \n");
+
+     for(i=0;i<3;i++)
+     {
+         printf("\n c[%d] = %d",i, c[i]);
+     }
+}
