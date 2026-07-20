5dc216ec6c5698a777fde703006e23829bdc9174
tanviba0404 <tanvibazala55@gmail.com>
2026-07-20 15:51:20 +0530

    Update author details and formatting in PROG1.c

--- Unit 1/PROG1.c
+++ Unit 1/PROG1.c
@@ -1,6 +1,6 @@
 /* Create an array of size 10,input values and print the array,and search an element in the array.
-Name:Daksh Zala
-Enrollment no.:92500527106 */
+Name:Tanviba Zala
+Enrollment no.:92500527103 */
 
 #include<stdio.h>
 
