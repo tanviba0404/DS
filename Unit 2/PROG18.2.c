5a386453698e5ccaf5e40daf5b06311f29d17ba6
tanviba0404 <tanvibazala55@gmail.com>
2026-08-17 14:40:26 +0530

    Update PROG18.2.c

--- Unit 2/PROG18.2.c
+++ Unit 2/PROG18.2.c
@@ -1,3 +1,7 @@
+/* Write a program to find Minimum and Maximum numbers from the given array with recursion.
+Name:Tanviba Zala
+Enrollment no.:92500527103 */
+
 #include<stdio.h>
 void findMinMax(int arr[],int n,int index,int *min,int *max);
 void main()
